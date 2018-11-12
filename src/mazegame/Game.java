cb93dee665f9c7d0cce03c240c96a198ac7bf1d0
Aldy-175314091 <>
2018-11-12 20:05:15 +0700

    versi 1.0

---
+++ src/mazegame/Game.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mazegame;
+
+/**
+ *
+ * @author Aldy
+ */
+public class Game {
+    private String nama = "Maze";
+    private Tempat lokasi;
+
+    public Game() {
+    }
+
+    public Game(Tempat lokasi) {
+        this.lokasi = lokasi;
+    }
+    
+    
+}
