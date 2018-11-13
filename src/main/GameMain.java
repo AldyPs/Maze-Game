5c13847498e845dc10405274dedf5477624e8eff
Arivandega_175314079 <>
2018-11-13 08:46:41 +0700

    versi1.1

--- src/main/GameMain.java
+++ src/main/GameMain.java
@@ -3,19 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Model;
+package main;
+
+import view.GameFrame;
+import view.TempatPanel;
 
 /**
  *
- * @author Aldy
+ * @author user only
  */
-public class MazeGame {
-
-    /**
-     * @param args the command line arguments
-     */
+public class GameMain {
     public static void main(String[] args) {
-        // TODO code application logic here
+        GameFrame game = new GameFrame("My Game");
     }
-    
 }
